4bd41271eb52b78413593e42ad7e5fc946d0ceb3
Gowdham Ranjithkumar <101965177+gowdhamaadhithya22@users.noreply.github.com>
2024-12-08 00:57:51 +0530

    Add files via upload

---
+++ fees_script.js
@@ -0,0 +1,97 @@
+function getInputValue(){
+
+    let inputVal=document.getElementById("uid").value;
+    let a=1001;
+
+    if(inputVal==1001)
+        {
+            document.getElementById("demo").innerHTML ="Aadhithya";
+            document.getElementById("demo1").innerHTML ="40000";
+        }
+    else if(inputVal==1002)
+        {
+            document.getElementById("demo").innerHTML ="Gowdham";
+            document.getElementById("demo1").innerHTML ="20000"; 
+        }
+    else if(inputVal==1003)
+        {
+            document.getElementById("demo").innerHTML ="Rajesh";
+            document.getElementById("demo1").innerHTML ="10000"; 
+        }
+    else if(inputVal==1004)
+        {
+            document.getElementById("demo").innerHTML ="Sathish";
+            document.getElementById("demo1").innerHTML ="10000"; 
+        }
+    else if(inputVal==1005)
+        {
+            document.getElementById("demo").innerHTML ="Priya";
+            document.getElementById("demo1").innerHTML ="10000"; 
+        }
+    else if(inputVal==1006)
+        {
+            document.getElementById("demo").innerHTML ="Dharsini";
+            document.getElementById("demo1").innerHTML ="20000"; 
+        }
+    else if(inputVal==1007)
+        {
+            document.getElementById("demo").innerHTML ="Raji";
+            document.getElementById("demo1").innerHTML ="10000"; 
+        }
+    else if(inputVal==1008)
+        {
+            document.getElementById("demo").innerHTML ="Lakshmini";
+            document.getElementById("demo1").innerHTML ="10000"; 
+        }
+    else if(inputVal==1009)
+        {
+            document.getElementById("demo").innerHTML ="Anitha";
+            document.getElementById("demo1").innerHTML ="10000"; 
+        }
+    else if(inputVal==1010)
+        {
+            document.getElementById("demo").innerHTML ="Gowtham";
+            document.getElementById("demo1").innerHTML ="20000"; 
+        }
+    else if(inputVal==1011)
+        {
+            document.getElementById("demo").innerHTML ="Adithi";
+            document.getElementById("demo1").innerHTML ="10000"; 
+        }
+    else if(inputVal==1012)
+        {
+            document.getElementById("demo").innerHTML ="Adhira";
+            document.getElementById("demo1").innerHTML ="10000"; 
+        }
+    else if(inputVal==1013)
+        {
+            document.getElementById("demo").innerHTML ="Mohana";
+            document.getElementById("demo1").innerHTML ="10000"; 
+        }
+    else if(inputVal==1014)
+        {
+            document.getElementById("demo").innerHTML ="Ramya";
+            document.getElementById("demo1").innerHTML ="20000"; 
+        }
+    else if(inputVal==1015)
+        {
+            document.getElementById("demo").innerHTML ="Jana";
+            document.getElementById("demo1").innerHTML ="10000"; 
+        }
+    else if(inputVal==1016)
+        {
+            document.getElementById("demo").innerHTML ="Dhivya";
+            document.getElementById("demo1").innerHTML ="10000"; 
+        }
+    else if(inputVal==1017)
+        {
+            document.getElementById("demo").innerHTML ="Deepa";
+            document.getElementById("demo1").innerHTML ="10000"; 
+        }
+    else{
+        document.getElementById("demo").innerHTML ="No Records Found";
+        document.getElementById("demo1").innerHTML ="0";
+    }
+    
+}
+
